be2bcaf6a5f24bcd7ad4f84225760df7292cd049
Yuki Takayama <s13rp041@icloud.com>
2020-02-26 00:24:17 +0900

    CORS用ミドルウェア作成

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Support\Facades\Log;
+use Closure;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        Log::INFO('CORS対策Middleware実行');
+        $response = $next($request);
+        $httpOrigin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : "";
+
+        Log::DEBUG("httpOrigin : " . $httpOrigin);
+
+        $response
+            ->header('Access-Control-Allow-Origin', 'http://localhost:3000')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+
+        return $response;
+    }
+}
